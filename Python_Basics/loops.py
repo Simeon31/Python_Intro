45af90c22214a40beaab10314f8afd1d0236a858
Simeon Markov <simeonmarkov777@gmail.com>
2025-06-14 21:24:48 +0300

    minor additions

--- Python_Basics/loops.py
+++ Python_Basics/loops.py
@@ -1,5 +1,23 @@
 i:int = 1
 
+# while loop
 while i <= 10:
     print(i * '*')
     i += 1
+print('-' * 10)
+
+#for loop
+
+# simple iteration
+for i in range(10):
+    print(i, (i + 1) * '.')
+print('-' * 10)
+
+# iteration with given range
+for i in range(1, 10):
+    print(i)
+print('-' * 10)
+
+# iteration with steps
+for i in range(1, 10, 2):
+    print(i)
--- Python_Basics/strings.py
+++ Python_Basics/strings.py
@@ -2,3 +2,10 @@ title = 'Python basics course'
 print(title.upper())
 print('-------------',)
 print('course' in title)
+
+print('*' * 10)
+# String concatenation
+first_name = "John"
+last_name = "Smith"
+full_name = f"{first_name} {last_name}"
+print(full_name)
