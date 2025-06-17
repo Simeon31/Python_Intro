db453269bc5517714f22de210c43ade4120688a9
Simeon Markov <simeonmarkov777@gmail.com>
2025-06-17 13:04:15 +0300

    working with stacks

---
+++ Python_Intermediate/stacks.py
@@ -0,0 +1,12 @@
+browsing_session = []
+
+browsing_session.append(1)
+browsing_session.append(2)
+browsing_session.append(3)
+browsing_session.append(4)
+
+print(browsing_session)
+
+browsing_session.pop()
+
+print(browsing_session)
