becf4faca5ab5ef76ac9f2ab5cffef783edb3cb9
Simeon M <simionsimion568@gmail.com>
2025-08-01 19:53:35 +0200

    adding templates

--- Python_Advanced/libraries/emails.py
+++ Python_Advanced/libraries/emails.py
@@ -1,12 +1,17 @@
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from string import Template
+from pathlib import Path
 import smtplib
+
+template = Template(Path("template.html").read_text())
 
 message = MIMEMultipart()
 message["From"] = "Simeon Markov"
 message["To"] = "testuser1273@outlook.com"
 message["Subject"] = "test subject"
-message.attach(MIMEText("test text", "plain"))
+body = template.substitute({"name": "Jonh"})
+message.attach(MIMEText(body, "html"))
 
 with smtplib.SMTP("smtp.gmail.com", 587) as smtp:
     smtp.ehlo()
