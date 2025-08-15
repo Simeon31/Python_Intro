c87936a55c587333d5407ef5e7231512aab8e8fa
Simeon M <simionsimion568@gmail.com>
2025-08-15 16:00:52 +0200

    url parameters

--- Web_Dev/videotech/movies/views.py
+++ Web_Dev/videotech/movies/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
 from .models import Movie
 
 # Create your views here.
@@ -9,3 +9,17 @@ def index(request):
     # return HttpResponse(output)
 
     return render(request, 'movies/index.html', {'movies': movies})
+
+def details(request, movie_id):
+    try:
+        movie = Movie.objects.get(id=movie_id)
+        return render(request, 'movies/details.html', {'movie': movie})
+    except Movie.DoesNotExist:
+        raise Http404
+
+    # Another shortcut way
+    # movie = get_object_or_404(klass=Movie, id=movie_id)
+    # return render(request, 'movies/details.html', {'movie': movie})
+
+
+
