d5898f720fb784b79deddac496bca1e00a02e068
Simeon M <simionsimion568@gmail.com>
2025-08-22 15:28:17 +0200

    Set a separate STATIC_ROOT and add STATICFILES_DIRS for collectstatic

--- Web_Dev/videotech/videotech/settings.py
+++ Web_Dev/videotech/videotech/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "movies.apps.MoviesConfig",
-    "api.apps.ApiConfig"
+    "videotech.movies.apps.MoviesConfig",
+    "videotech.api.apps.ApiConfig"
 ]
 
 MIDDLEWARE = [
